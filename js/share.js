ac7358a1a420b1fffba8251dd5d8129eb7409888
v-ranch <gaji960405@gmail.com>
2021-11-03 00:02:06 +0900

    1103 share update

--- js/share.js
+++ js/share.js
@@ -3,7 +3,7 @@ const url = 'https://movemeongt.netlify.app/';
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '지금 당신에게 무브멍트가 추천하는 멍은?'
+  const shareTitle = '당신에게 무브멍트가 추천하는 멍은?'
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.jpg';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
